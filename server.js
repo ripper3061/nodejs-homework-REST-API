05a3125759ba4c6ad8a3008289118a45db458ecb
Vladimir Ponomarov <ripper3061@gmail.com>
2023-07-11 13:18:48 +0300

    connect db-contacts with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./app')
+// const app = require('./app')
+const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = 'mongodb+srv://ripper3061:2RDT3Cd1EaF1KYEw@db-contacts.ucmkhi8.mongodb.net/db-contacts?retryWrites=true&w=majority'
+mongoose.connect(DB_HOST).then(()=>{console.log("Database connection successful")}).catch((err) => {console.error(err.message)})
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
