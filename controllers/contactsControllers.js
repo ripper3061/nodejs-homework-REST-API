d1f5734d4760db5b5abc3747bae4327ec5f6728c
Vladimir Ponomarov <ripper3061@gmail.com>
2023-07-11 15:46:21 +0300

    add route and controller updateStatusContactController

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -73,10 +73,34 @@ const getContactByIdController =  async (req, res, next) => {
     }
   }
 
+  const updateStatusContactController = async (req, res, next) => {
+    try {
+      const { favorite } = req.body;
+      if (!("favorite" in req.body)) {
+        return next(res.status(404).json({ message: "missing field favorite" }));
+      }
+      const { contactId } = req.params;
+      const contactById = await Contact.findById(contactId);
+      if (!contactById) {
+        return next(res.status(404).json({ message: "Not found" }));
+      }
+  
+      const updatedStatusContact = await Contact.findByIdAndUpdate(
+        contactId,
+        { favorite },
+        { new: true }
+      );
+      return res.status(200).json(updatedStatusContact);
+    } catch (error) {
+      next(error);
+    }
+  };
+
   module.exports = {
     getListContactsController,
     getContactByIdController,
     deleteContactController,
     createNewContactController,
     updateContactController,
+    updateStatusContactController,
   };
