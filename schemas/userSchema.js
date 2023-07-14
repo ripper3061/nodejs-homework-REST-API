6cfa6ebe5ddee9398bc0aaab17bff8f85d914c4a
Vladimir Ponomarov <ripper3061@gmail.com>
2023-07-14 11:55:29 +0300

    step 1 - user schema and model

---
+++ schemas/userSchema.js
@@ -0,0 +1,18 @@
+
+const Joi = require("joi");
+
+const userValidateSchema = Joi.object({
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .min(4)
+    .required(),
+  password: Joi.string().required().min(3).regex(/^[a-zA-Z0-9]{3,30}$/),
+  subscription: Joi.string().optional(),
+});
+
+module.exports = {
+  userValidateSchema,
+};
